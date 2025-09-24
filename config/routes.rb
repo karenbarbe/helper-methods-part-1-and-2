c728186c3c3ea73e5185047476feb1187edbd123
Karen Barbé <info@karenbarbe.com>
2025-09-24 11:18:44 -0500

    refactor movie routes to include named path helpers

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies # Creates methods movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie # Creates methods new_movie_url and new_movie_path
 
   # READ
   get "/movies" => "movies#index"
-  get "/movies/:id" => "movies#show"
+  get "/movies/:id" => "movies#show", as: :movie # Creates movie_path() Expects an argument to populate :id segment
 
   # UPDATE
   patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie # Creates edit_movie_path() Expects an argument to populate :id segment
 
   # DELETE
   delete "/movies/:id" => "movies#destroy"
